84c6ef6b64e41f37759c8c70d58e946e60472a32
EngBetao <humberto.software@gmail.com>
2026-03-23 17:51:04 -0300

    feat: service do supabase finalizado e sem erros de tipagem

--- backend/common/supabase.service.ts
+++ backend/common/supabase.service.ts
@@ -4,16 +4,15 @@ import { createClient, SupabaseClient } from '@supabase/supabase-js';
 
 @Injectable()
  export class SupabaseService {
-  private supabase: SupabaseClient;
+  private supabase: any;
 
   constructor() {
-    const supabaseUrl = process.env.SUPABASE_URL || '';
-    const supabaseKey = process.env.SUPABASE_KEY || '';
+    const supabaseUrl = process.env.SUPABASE_URL!;
+    const supabaseKey = process.env.SUPABASE_KEY!;
 
     this.supabase = createClient(supabaseUrl, supabaseKey);
   }
-
-  getClient() {
+  getClient(): any {
     return this.supabase;
   }
 }
