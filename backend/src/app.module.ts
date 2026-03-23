efaec369736c4c47c534a103b3f75387d90d4c71
EngBetao <humberto.software@gmail.com>
2026-03-23 17:34:09 -0300

    feat: backend conectado ao supabase com estrutura organizada

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SupabaseService } from '../common/supabase.service'; // Importe aqui
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, SupabaseService],
 })
 export class AppModule {}
